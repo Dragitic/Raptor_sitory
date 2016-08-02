0f61015373c1f0e6f0908ce8772491a26ebd7d83
Mateusz Drażyk <mdrazyk92@gmail.com>
2016-08-02 23:17:52 +0200

    Added UnitTests

---
+++ CageMaster/CageMaster/Tests/TestClass.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using CageMaster.Animals;
+using CageMaster.Constants;
+using CageMaster.PrisonHolder;
+using NUnit.Framework;
+
+namespace CageMaster.Tests
+{
+    [TestFixture]
+    public class TestClass
+    {
+        [Test]
+        public void ShouldImprisonCatAndRealiseBird()
+        {
+            //given
+            Animal cat = new AnimalFactory().Create(StringsDontDoThisNormally.Housecat);
+            Animal bird = new AnimalFactory().Create(StringsDontDoThisNormally.Bird);
+            Prison prison = new PrisonFactory().Create(StringsDontDoThisNormally.CatPrison, StringsDontDoThisNormally.Small);
+
+            //when
+            prison.Catch(cat);
+            prison.Catch(bird);
+            List<Animal> escapees = prison.Escaped();
+            List<Animal> imprisoned = prison.Held();
+
+            //than  
+            foreach (var escapee in escapees)
+            {
+                Assert.AreSame(escapee, bird);
+            }
+
+            foreach (var imprison in imprisoned)
+            {
+                Assert.AreSame(imprison, cat);
+            }
+        }
+    }
+}
